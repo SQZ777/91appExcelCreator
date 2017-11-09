769333b10a800f6dc9b9fcaf58b9ba317f199380
darrenzhang <darrenzhang@91app.com>
2017-11-09 13:47:55 +0800

    red some column

--- 91appExcelCreator/Form1.Designer.cs
+++ 91appExcelCreator/Form1.Designer.cs
@@ -108,6 +108,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
             this.label1.Location = new System.Drawing.Point(120, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(91, 24);
@@ -118,6 +119,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
             this.label2.Location = new System.Drawing.Point(120, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(91, 24);
@@ -128,6 +130,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
             this.label3.Location = new System.Drawing.Point(120, 103);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(91, 24);
@@ -353,6 +356,7 @@
             // 
             this.label25.AutoSize = true;
             this.label25.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold);
+            this.label25.ForeColor = System.Drawing.Color.Red;
             this.label25.Location = new System.Drawing.Point(499, 404);
             this.label25.Name = "label25";
             this.label25.Size = new System.Drawing.Size(91, 24);
